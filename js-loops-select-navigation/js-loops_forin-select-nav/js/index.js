331273084a44c22b1c8dee35bb9258db47197fc0
Gregor Haslinger <kontakt@gregorsart.de>
2023-07-27 11:12:58 +0200

    add (select and tricky navigation)

---
+++ js-loops-select-navigation/js-loops_forin-select-nav/js/index.js
@@ -0,0 +1,59 @@
+"use strict";
+console.clear();
+console.log("hello newbie");
+
+const main = document.querySelector('[data-js="main"]');
+
+// Part 1: Creating a Select Input
+
+const languages = {
+  DE: "German",
+  EN: "English",
+  ES: "Spanish",
+  FR: "French",
+  IT: "Italian",
+  NL: "Dutch",
+  PT: "Portuguese",
+  RU: "Russian",
+  ZH: "Chinese",
+};
+
+const select = document.createElement("select");
+select.name = "languages";
+main.append(select);
+
+// --v-- write/change code here --v--
+
+for (const language in languages) {
+  const option = document.createElement("option");
+  option.textContent = languages[language];
+  select.append(option);
+}
+
+// --^-- write/change code here --^--
+
+// Part 2: Creating a Navigation Bar
+
+const nav = {
+  home: { href: "/home", text: "Home" },
+  about: { href: "/about", text: "About" },
+  contact: { href: "/contact", text: "Contact" },
+};
+
+const navElement = document.createElement("nav");
+const ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul);
+
+// --v-- write/change code here --v--
+
+for (const navItem in nav) {
+  const navLiItem = document.createElement("li");
+  const linkELement = document.createElement("a");
+  navLiItem.append(linkELement);
+  ul.append(navLiItem);
+  linkELement.textContent = nav[navItem].text;
+  linkELement.setAttribute("href", nav[navItem].href);
+}
+
+// --^-- write/change code here --^--
