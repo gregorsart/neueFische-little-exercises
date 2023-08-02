6a1c7bac8d059e9d2241176ef4ec198bd6c28fd1
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-02 11:41:02 +0200

    add (js structure quiz app solved)

---
+++ js-structure-quiz-app/js-structure_quiz-app/index.js
@@ -0,0 +1,32 @@
+import App from "./components/App/App.js";
+
+// Store the cards in a global state
+
+// -------------------------------------------------------------
+
+// cards data object was here ----------------------------------
+
+// header was here ---------------------------------------------
+
+// handle form submit was here ---------------------------------
+
+// form was here -----------------------------------------------
+
+// bookmark was here -------------------------------------------
+
+// function card was here --------------------------------------
+
+// function card  list was here --------------------------------
+
+// app was here ------------------------------------------------
+
+// -------------------------------------------------------------
+
+// The render function renders the app to the DOM
+
+function render() {
+  const root = document.querySelector("#root");
+  root.append(App());
+}
+
+render();
