4c80a12799f13fb8e49aba97bc33769a35f77f7e
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-03 12:13:24 +0200

    add (js methods, news flash exercise solved)

---
+++ js-methods-news-flash/js-array-methods-2_news-flash/index.js
@@ -0,0 +1,25 @@
+import { news } from "./utils/news.js";
+import { Card } from "./components/Card/Card.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+const filteredNews = news.filter((newsItem) =>
+  newsItem.categories.includes("politics")
+);
+
+// Part 2 - start here
+const sortedNews = filteredNews;
+sortedNews.sort((a, b) => {
+  return a.body.length - b.body.length;
+});
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+checkSortedNews(sortedNews);
