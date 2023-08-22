3ecc194157eb445eced6f71fe9c52cc9f5588ff3
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-22 11:08:45 +0200

    add (exercise react state favourite button)

--- react-state-journal-app-favourite-button/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ react-state-journal-app-favourite-button/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,15 +1,17 @@
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
+import { useState } from "react";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setIsFavourite] = useState(false);
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
+        setIsFavourite(!isFavorite);
         console.log("favorite button clicked");
       }}
       aria-label="favorite"
