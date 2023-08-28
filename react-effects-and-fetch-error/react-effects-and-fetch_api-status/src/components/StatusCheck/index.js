705093a613550864efb55debdaa10e3867520b7c
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-28 12:17:20 +0200

    add (error handling exercise)

---
+++ react-effects-and-fetch-error/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./StatusCheck.css";
+
+const apiStatusUrl = "https://example-apis.vercel.app/api/status";
+
+export default function StatusCheck() {
+  const [statusIcon, setStatusIcon] = useState("⁉️");
+  // Something needs to change here…
+  // ↙️
+  async function handleCheckApiStatus() {
+    setStatusIcon("⏳");
+    const response = await fetch(apiStatusUrl);
+
+    if (response.ok) setStatusIcon("✅");
+    else {
+      setStatusIcon("❌");
+    }
+  }
+
+  return (
+    <article className="status-check">
+      <div className="status-check__wrapper">
+        <h2 className="status-check__heading">Status:</h2>
+        <span className="status-check__icon">{statusIcon}</span>
+      </div>
+      <button
+        type="button"
+        className="status-check__button"
+        onClick={handleCheckApiStatus}
+      >
+        Check API Status
+      </button>
+    </article>
+  );
+}
