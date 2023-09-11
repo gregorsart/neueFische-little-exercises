6ce590d3eff98f28da96d069f1aa972d9a5679ee
Gregor Haslinger <kontakt@gregorsart.de>
2023-09-11 11:33:49 +0200

    add (react data fetching iss tracker)

---
+++ react-data-fetching-iss-tracker/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function ISSTracker() {
+  const { data, mutate, error, isLoading } = useSWR(URL, {
+    refreshInterval: 5000,
+  });
+  console.log("data---", data);
+
+  // const [coords, setCoords] = useState({
+  //   longitude: 0,
+  //   latitude: 0,
+  // });
+
+  // async function getISSCoords() {
+  //   try {
+  //     const response = await fetch(URL);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     getISSCoords();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
+
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
---
+++ react-data-fetching-iss-tracker/react-data-fetching_iss-tracker/pages/_app.js
@@ -0,0 +1,34 @@
+import { SWRConfig } from "swr";
+import GlobalStyle from "../styles";
+
+export default function App({ Component, pageProps }) {
+  // WITHOUT ERROR HANDLING
+
+  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  // WITH ERROR HANDLING
+
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      // Attach extra info to the error object.
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
+  return (
+    <>
+      <GlobalStyle />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
+  );
+}
